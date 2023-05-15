c1e60d02fd4ed247727630d1d38a6140b4f1385a
iscodand <iscodand@outlook.com>
2023-05-15 16:42:42 -0300

    update: defining non nullable profile fields

--- MyLifeApp.Domain/Entities/Profile.cs
+++ MyLifeApp.Domain/Entities/Profile.cs
@@ -1,22 +1,32 @@
 ﻿using Identity.Infrastructure.Models;
 using MyLifeApp.Domain.Common;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyLifeApp.Domain.Entities
 {
     public class Profile : BaseEntity
     {
+        [ForeignKey("UserId")]
         public User User { get; set; }
-        public string Bio { get; set; }
-        public string Location { get; set; }
-        public DateTime BirthDate { get; set; }
+        public string UserId { get; set; }
+        
+        public string? Bio { get; set; }
+        public string? Location { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsPrivate { get; set; }
         public bool IsActive { get; set; }
-        public ICollection<ProfileFollower> ProfileFollowers { get; set; }
-        public ICollection<ProfileFollower> ProfileFollowing { get; set; }
-        public ICollection<Post> Posts { get; set; }
-        public ICollection<PostLike> PostLikes { get; set; }
-        public ICollection<PostComment> PostComments { get; set; }
+        public ICollection<ProfileFollower>? ProfileFollowers { get; set; }
+        public ICollection<ProfileFollower>? ProfileFollowing { get; set; }
+        public ICollection<Post>? Posts { get; set; }
+        public ICollection<PostLike>? PostLikes { get; set; }
+        public ICollection<PostComment>? PostComments { get; set; }
+
+        public Profile()
+        {
+            CreatedAt = DateTime.Now;
+            IsActive = true;
+            IsPrivate = false;
+        }
     }
 
     public class ProfileAnalytics
