d9d154c6a8312b37d2bcaf628c8c4ae0d889dc64
iscodand <iscodand@outlook.com>
2023-05-15 16:43:04 -0300

    add: BaseRepository template

---
+++ MyLifeApp.Infrastructure.Data/repositories/BaseRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLifeApp.Infrastructure.Data.Repositories
+{
+    // Refactor => Verify the implementation of an base repository (interface or abstract class?)
+    public abstract class BaseRepository
+    {
+    }
+}
