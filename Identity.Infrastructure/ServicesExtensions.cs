1d2eb23e7e8666bc032e1d49789efc94fb68cacd
iscodand <iscodand@outlook.com>
2023-05-15 16:33:58 -0300

    to-do: configure ServicesExtensions.cs

--- Identity.Infrastructure/ServicesExtensions.cs
+++ Identity.Infrastructure/ServicesExtensions.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Identity.Infrastructure.Interfaces;
+using Identity.Infrastructure.Models;
+using Identity.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Identity.Infrastructure
 {
-    public class ServicesExtensions
+    public static class ServicesExtensions
     {
         // TO-DO: Add services extensions for Identity.Infra
+        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Services
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ITokenRepository, TokenRepository>();
+            #endregion
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"])),
+                    ValidateIssuerSigningKey = true
+                };
+            });
+        }
     }
 }
