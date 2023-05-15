6b933987824529f487b7e488fd1e056cb3f73de6
iscodand <iscodand@outlook.com>
2023-05-15 16:42:12 -0300

    add: mapping profiles for AutoMapper

---
+++ MyLifeApp.Application/Mappings/PostsMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using MyLifeApp.Application.Dtos.Requests.Profile;
+
+namespace MyLifeApp.Application.Mappings
+{
+    public class PostsMapping : Profile
+    {
+        public PostsMapping()
+        {
+        }
+    }
+}
---
+++ MyLifeApp.Application/Mappings/ProfilesMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyLifeApp.Application.Dtos.Requests.Profile;
+
+namespace MyLifeApp.Application.Mappings
+{
+    public class ProfilesMapping : Profile
+    {
+        public ProfilesMapping()
+        {
+            CreateMap<Domain.Entities.Profile, UpdateProfileRequest>();
+            CreateMap<UpdateProfileRequest, Domain.Entities.Profile>();
+        }
+    }
+}
