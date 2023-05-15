d7ec69ec3b98f55fbfed102514d0a17c51364be2
iscodand <iscodand@outlook.com>
2023-05-15 16:40:48 -0300

    update: WebApi settings

--- MyLife.Api/Program.cs
+++ MyLife.Api/Program.cs
@@ -5,23 +5,33 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using MyLifeApp.Application.Interfaces;
 using MyLifeApp.Infrastructure.Data.Context;
+using MyLifeApp.Infrastructure.Data.Repositories;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Ignore null fields from Response
+//    .AddJsonOptions(options =>
+//    {
+//        options.JsonSerializerOptions.DefaultIgnoreCondition =
+//               System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+//    });
 
 // Dependency Injection
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 // Configure Database Context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -29,6 +39,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Identity Settings
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
     options.Password.RequiredLength = 8;
