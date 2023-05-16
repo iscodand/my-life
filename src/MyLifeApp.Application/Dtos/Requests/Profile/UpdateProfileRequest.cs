a5ff47042721bd8510e869487ea8d1f4dcd77191
iscodand <iscodand@outlook.com>
2023-05-16 15:28:46 -0300

    add: validation for requests

--- src/MyLifeApp.Application/Dtos/Requests/Profile/UpdateProfileRequest.cs
+++ src/MyLifeApp.Application/Dtos/Requests/Profile/UpdateProfileRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace MyLifeApp.Application.Dtos.Requests.Profile
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLifeApp.Application.Dtos.Requests.Profile
 {
     public class UpdateProfileRequest
     {
+        [StringLength(90)]
         public string? Bio { get; set; }
+
+        [StringLength(90)]
         public string? Location { get; set; }
+        
         public DateTime BirthDate { get; set; }
         public bool IsPrivate { get; set; }
     }
