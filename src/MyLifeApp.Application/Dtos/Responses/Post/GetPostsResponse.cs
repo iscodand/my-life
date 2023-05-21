9bc30c844f02be599195ee4debec797820f02fdb
iscodand <iscodand@outlook.com>
2023-05-21 03:27:13 -0300

    update: get posts responses

--- src/MyLifeApp.Application/Dtos/Responses/Post/GetPostsResponse.cs
+++ src/MyLifeApp.Application/Dtos/Responses/Post/GetPostsResponse.cs
@@ -2,10 +2,11 @@
 
 namespace MyLifeApp.Application.Dtos.Responses.Post
 {
-    public class GetPostsResponse : BaseResponse
+    public class GetPostsResponse
     {
-        public string Title { get; set; }
-        public GetProfileResponse Profile { get; set; }
+        public string? Title { get; set; }
+        public GetProfileResponse? Profile { get; set; }
         public int Likes { get; set; }
+        public int Comments { get; set; }
     }
 }
