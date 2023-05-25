2ea5eb6dfcc399fe7751f75bba7c48ddb48c729b
iscodand <iscodand@outlook.com>
2023-05-25 16:50:06 -0300

    add: nullable fields

--- src/MyLifeApp.Application/Dtos/Responses/BaseResponse.cs
+++ src/MyLifeApp.Application/Dtos/Responses/BaseResponse.cs
@@ -2,8 +2,8 @@
 {
     public class BaseResponse
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public bool IsSuccess { get; set; }
-        public IEnumerable<string> Errors { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
     }
 }
