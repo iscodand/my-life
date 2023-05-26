e4f8c1be205545bcfc78798e8564ebd9e06e5dac
iscodand <iscodand@outlook.com>
2023-05-26 14:32:40 -0300

    add: Dependency Injection things

--- src/MyLife.Api/Program.cs
+++ src/MyLife.Api/Program.cs
@@ -28,12 +28,11 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 
 // Dependency Injection
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
-//builder.Services.AddScoped<IBaseRepository, BaseRepository>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
-// Refactoring things
-builder.Services.AddScoped<IRefactorPostRepository, RefactorPostRepository>();
-builder.Services.AddScoped<IRefactorProfileRepository, RefactorProfileRepository>();
 builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
+// Refactoring things
+builder.Services.AddScoped<IRefactorProfileRepository, RefactorProfileRepository>();
 //
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
