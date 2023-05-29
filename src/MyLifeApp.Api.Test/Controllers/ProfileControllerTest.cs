22ddbc6ab655669840b9a3e9db8fe0ddaa0c3611
iscodand <iscodand@outlook.com>
2023-05-29 16:54:56 -0300

    tests: ProfileController Unit Tests

--- src/MyLifeApp.Api.Test/Controllers/ProfileControllerTest.cs
+++ src/MyLifeApp.Api.Test/Controllers/ProfileControllerTest.cs
@@ -1,12 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using FluentAssertions;
+using FakeItEasy;
+using MyLifeApp.Application.Interfaces.Services;
+using MyLifeApp.Application.Dtos.Responses.Profile;
+using MyLifeApp.Application.Dtos.Responses;
+using MyLifeApp.Domain.Entities;
+using MyLife.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Identity.Infrastructure.Models;
+using MyLifeApp.Application.Dtos.Requests.Profile;
 
 namespace MyLifeApp.Api.Test.Controllers
 {
-    internal class ProfileControllerTest
+    public class ProfileControllerTest
     {
+        private readonly IProfileService _profileService;
+        private readonly ProfileController _controller;
+
+        public ProfileControllerTest()
+        {
+            _profileService = A.Fake<IProfileService>();
+            _controller = new ProfileController(_profileService);
+        }
+
+        // => verify how implement this unit test
+        // [Fact]
+        // public Task GetAuthenticatedProfile_ReturnsOk()
+        // {
+        // }
+
+        [Fact]
+        public async Task GetProfileByUsername_ExistentProfile_ReturnsOk()
+        {
+            // Arrange
+            var profile = A.Fake<Profile>();
+            var user = A.Fake<User>();
+            profile.User = user;
+
+            DetailProfileResponse response = new()
+            {
+                Id = profile!.Id,
+                Name = profile.User?.Name,
+                Username = profile.User?.UserName,
+                Bio = profile.Bio,
+                BirthDate = profile.BirthDate,
+                IsPrivate = profile.IsPrivate,
+                Message = "Success",
+                IsSuccess = true
+            };
+
+            A.CallTo(() => _profileService.GetProfileByUsernameAsync(profile.User!.UserName!))
+                .Returns(Task.FromResult(response));
+
+            // Act
+            var result = await _controller.GetProfileByUsername(profile.User!.UserName!);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(response);
+        }
+
+        [Fact]
+        public async Task GetProfileByUsername_InexistentProfile_ReturnsNotFound()
+        {
+            // Arrange
+            string inexistentUsername = "inexistent";
+
+            DetailProfileResponse response = new()
+            {
+                IsSuccess = false
+            };
+
+            A.CallTo(() => _profileService.GetProfileByUsernameAsync(inexistentUsername))
+                .Returns(Task.FromResult(response));
+
+            // Act
+            var result = await _controller.GetProfileByUsername(inexistentUsername);
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task UpdateProfile_ExistentProfile_ReturnsOk()
+        {
+            // Arrange
+            UpdateProfileRequest request = new()
+            {
+                Bio = "new bio! yay",
+                Location = "IDK my location",
+                BirthDate = new DateTime(),
+                IsPrivate = false
+            };
+
+            BaseResponse response = new()
+            {
+                Message = "Profile Successfuly Updated",
+                IsSuccess = true
+            };
+
+            A.CallTo(() => _profileService.UpdateProfileAsync(A<UpdateProfileRequest>.Ignored))
+                .Returns(Task.FromResult(response));
+
+            // Act
+            var result = await _controller.UpdateProfile(request);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeEquivalentTo(response);
+        }
     }
 }
