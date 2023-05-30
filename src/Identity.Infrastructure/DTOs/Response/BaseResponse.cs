41803f27ccda2fa169fa8606359a95497c1d8fe5
iscodand <iscodand@outlook.com>
2023-05-30 12:31:54 -0300

    add: StatusCode property for BaseResponse

--- src/Identity.Infrastructure/DTOs/Response/BaseResponse.cs
+++ src/Identity.Infrastructure/DTOs/Response/BaseResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace Identity.Infrastructure.DTOs.Response
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Infrastructure.DTOs.Response
 {
     public class BaseResponse
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public bool IsSuccess { get; set; }
-        public IEnumerable<string> Errors { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public IEnumerable<string>? Errors { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+        public int StatusCode { get; set; }
     }
 }
--- src/MyLifeApp.Application/Dtos/Responses/BaseResponse.cs
+++ src/MyLifeApp.Application/Dtos/Responses/BaseResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace MyLifeApp.Application.Dtos.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace MyLifeApp.Application.Dtos.Responses
 {
     public class BaseResponse
     {
         public string? Message { get; set; }
         public bool IsSuccess { get; set; }
+        
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public IEnumerable<string>? Errors { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+        public int StatusCode { get; set; }
     }
 }
