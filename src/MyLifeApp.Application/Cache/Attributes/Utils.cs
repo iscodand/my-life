2c50ffde74f1921721d2bcf5625f1f347e2e139f
iscodand <iscodand@outlook.com>
2023-07-07 06:22:30 -0300

    add: utils for Cache Attributes

---
+++ src/MyLifeApp.Application/Cache/Attributes/Utils.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace MyLifeApp.Application.Cache.Attributes
+{
+    public class Utils
+    {
+        public static string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+
+            foreach (var (key, value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
