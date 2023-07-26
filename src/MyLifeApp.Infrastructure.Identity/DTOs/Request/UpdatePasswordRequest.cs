faec85dcec0651c6f3fa17ae0962bcfb92e12c43
iscodand <iscodand@outlook.com>
2023-07-26 20:01:39 -0300

    add: validations with data annotations

--- src/MyLifeApp.Infrastructure.Identity/DTOs/Request/UpdatePasswordRequest.cs
+++ src/MyLifeApp.Infrastructure.Identity/DTOs/Request/UpdatePasswordRequest.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyLifeApp.Infrastructure.Identity.DTOs.Request
 {
     public class UpdatePasswordRequest
     {
-        public string OldPassword { get; set; }
-        public string NewPassword { get; set; }
-        public string ConfirmNewPassword { get; set; }
+        [Required(ErrorMessage = "Your Old Password is required")]
+        public string? OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Your New Password is required")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Password must higher than 8 characters.")]
+        public string? NewPassword { get; set; }
+
+        [Compare("NewPassword")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Password must higher than 8 characters.")]
+        public string? ConfirmNewPassword { get; set; }
     }
 }
