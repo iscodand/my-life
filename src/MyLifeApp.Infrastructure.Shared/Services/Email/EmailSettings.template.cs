f48c2f267f622f32976665686f11fc9392792fdc
iscodand <iscodand@outlook.com>
2023-07-27 15:40:10 -0300

    add: new EmailService

---
+++ src/MyLifeApp.Infrastructure.Shared/Services/Email/EmailSettings.template.cs
@@ -0,0 +1,14 @@
+namespace MyLifeApp.Infrastructure.Shared.Services.Email
+{
+    // NOTES:
+    // Change filename to EmailSettings.cs
+    // Change `EmailSettingsTemplate` for `EmailSettings`
+    // Insert Email, Password, Port and Host of Email to be used
+    public class EmailSettingsTemplate
+    {
+        public readonly string Email = "";
+        public readonly string Password = "";
+        public readonly int Port = 0;
+        public readonly string Host = "";
+    }
+}
